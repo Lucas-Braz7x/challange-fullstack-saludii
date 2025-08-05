7b7830492488629c82e3769d5b42304b55364ba4
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-08-05 15:27:35 -0300

    :sparkles: feat: adicionando modal de cadastro de receita

---
+++ web/src/components/organisms/Modal/contants/index.ts
@@ -0,0 +1,13 @@
+export const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '70%',
+  maxHeight: '90vh',
+  overflowY: 'auto',
+  bgcolor: 'background.paper',
+  borderRadius: '25px',
+  boxShadow: 24,
+  p: 4,
+}
---
+++ web/src/components/organisms/Modal/index.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react'
+
+import { Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import { CreateReceitaInput } from 'types/graphql'
+
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+
+import { CREATE_RECIPE_MUTATION } from 'src/requests'
+
+import { RecipeForm } from '../RecipeForm'
+
+import { style } from './contants'
+
+export const CreateRecipeModal = () => {
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const [createReceita, { loading, error }] = useMutation(
+    CREATE_RECIPE_MUTATION,
+    {
+      onCompleted: () => {
+        toast.success('Receita created')
+        //navigate(routes.receitas())
+      },
+      onError: (error) => {
+        console.log(error)
+        toast.error('TESTE')
+      },
+    }
+  )
+
+  const onSave = (input: CreateReceitaInput) => {
+    console.log({ input })
+    createReceita({
+      variables: { input },
+    })
+  }
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" mb={2}>
+            Criar uma nova receita
+          </Typography>
+          <RecipeForm
+            onSave={onSave}
+            loading={loading}
+            error={error}
+            handleClose={handleClose}
+          />
+        </Box>
+      </Modal>
+    </div>
+  )
+}
